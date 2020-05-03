f6f176b996f899c20c024ab158db791da453dc6d
Guinsly Mondesir <guinslym@gmail.com>
2020-05-03 17:36:35 -0400

    screenshots

--- lib/screens/theame.dart
+++ lib/screens/theame.dart
@@ -9,6 +9,8 @@ Color secondaryButtonColor = HexColor("212f3c");
 
 TextStyle headlines = TextStyle(letterSpacing: 2.0,fontSize: 15, color: HexColor("d3d3d3"));
 TextStyle boldNumber = TextStyle(color: HexColor("fbfbfb"), fontWeight: FontWeight.bold,fontSize: 50.0);
+TextStyle boldNumberRed = TextStyle(color: Colors.red, fontWeight: FontWeight.bold,fontSize: 50.0);
+TextStyle headlineTitle = TextStyle(color: HexColor("fbfbfb"), fontWeight: FontWeight.bold,fontSize: 30.0);
 TextStyle secondaryButtonColorStyle = TextStyle(color: secondaryButtonColor, fontWeight: FontWeight.bold,fontSize: 26.0);
 TextStyle primaryButtonStyle = TextStyle( fontSize: 18, fontWeight: FontWeight.normal, letterSpacing: 1,wordSpacing: 2, color: secondaryButtonColor);
 TextStyle resultNumber = TextStyle(color: HexColor("fbfbfb"), fontWeight: FontWeight.bold,fontSize: 80.0,letterSpacing: 0.1);
