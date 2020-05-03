d11ad5c9dfe0af766fbf0b1eb2fb2c3f9e4b5247
Guinsly Mondesir <guinslym@gmail.com>
2020-05-03 14:49:36 -0400

    initial commit

---
+++ android/app/src/main/kotlin/com/example/sp_ask_dashboard_mobile_app_layout/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sp_ask_dashboard_mobile_app_layout
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
