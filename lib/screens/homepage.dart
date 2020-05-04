b5b1514449044c31837c042766d009c2fafc520b
Guinsly Mondesir <guinslym@gmail.com>
2020-05-04 17:34:12 -0400

    refactoring

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -55,73 +55,9 @@ class _MyAppState extends State<MyApp> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 // Services
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    margin: EdgeInsets.all(10.0),
-                    //height: MediaQuery.of(context).size.height * 0.25,
-                    decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            //color: Color(0xff09081D),
-                            offset: Offset(
-                              5.0, // horizontal, move right 10
-                              5.0, // vertical, move down 10
-                            ),
-                          )
-                        ],
-                        borderRadius: BorderRadius.circular(10.0),
-                        color: primary),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        //Image.asset('images/male.png',height: 100.0,width: 100.0,),
-                        SizedBox(
-                          height: 40.0,
-                        ),
-                        Text('Today', style: headlines),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text("92", style: boldNumber),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    margin: EdgeInsets.all(10.0),
-                    //height: MediaQuery.of(context).size.height * 0.25,
-                    decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            offset: Offset(
-                              5.0, // horizontal, move right 10
-                              5.0, // vertical, move down 10
-                            ),
-                          )
-                        ],
-                        borderRadius: BorderRadius.circular(10.0),
-                        color: primary),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        //Image.asset('images/female.png',height: 100.0,width: 100.0,),
-                        SizedBox(
-                          height: 40.0,
-                        ),
-                        Text('This month', style: headlines),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text("1,457", style: boldNumber),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                
+                AskServiceWidget(),
+                AskServiceWidget(),
               ],
             ),
             Padding(
@@ -139,106 +75,9 @@ class _MyAppState extends State<MyApp> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    margin: EdgeInsets.all(10.0),
-                    //height: MediaQuery.of(context).size.height * 0.25,
-                    decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            offset: Offset(
-                              5.0, // horizontal, move right 10
-                              5.0, // vertical, move down 10
-                            ),
-                          )
-                        ],
-                        borderRadius: BorderRadius.circular(10.0),
-                        color: primary),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        //Image.asset('images/male.png',height: 100.0,width: 100.0,),
-                        SizedBox(
-                          height: 40.0,
-                        ),
-                        Text('Web', style: headlines),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text("On", style: boldNumber),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    margin: EdgeInsets.all(10.0),
-                    //height: MediaQuery.of(context).size.height * 0.25,
-                    decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            offset: Offset(
-                              5.0, // horizontal, move right 10
-                              5.0, // vertical, move down 10
-                            ),
-                          )
-                        ],
-                        borderRadius: BorderRadius.circular(10.0),
-                        color: primary),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        //Image.asset('images/male.png',height: 100.0,width: 100.0,),
-                        SizedBox(
-                          height: 40.0,
-                        ),
-                        Text('Clavardez', style: headlines),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text("On", style: boldNumber),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    margin: EdgeInsets.all(10.0),
-                    //height: MediaQuery.of(context).size.height * 0.25,
-                    decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            offset: Offset(
-                              5.0, // horizontal, move right 10
-                              5.0, // vertical, move down 10
-                            ),
-                          )
-                        ],
-                        borderRadius: BorderRadius.circular(10.0),
-                        color: primary),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        //Image.asset('images/male.png',height: 100.0,width: 100.0,),
-                        SizedBox(
-                          height: 40.0,
-                        ),
-                        Text('SMS', style: headlines),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text("Off",
-                              style: TextStyle(
-                                  color: Colors.red,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 50.0)),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                AskServiceWidget(),
+                AskServiceWidget(),
+                AskServiceWidget(),
               ],
             ),
             Padding(
@@ -252,43 +91,57 @@ class _MyAppState extends State<MyApp> {
             ),
             Row(
               children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    margin: EdgeInsets.all(10.0),
-                    //height: MediaQuery.of(context).size.height * 0.25,
-                    decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            offset: Offset(
-                              5.0, // horizontal, move right 10
-                              5.0, // vertical, move down 10
-                            ),
-                          )
-                        ],
-                        borderRadius: BorderRadius.circular(10.0),
-                        color: primary),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        //Image.asset('images/male.png',height: 100.0,width: 100.0,),
-                        SizedBox(
-                          height: 40.0,
-                        ),
-                        Text('Operators', style: headlines),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text("5",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 50.0)),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+              
+              AskServiceWidget(),
+
+
               ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AskServiceWidget extends StatelessWidget {
+  const AskServiceWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        margin: EdgeInsets.all(10.0),
+        //height: MediaQuery.of(context).size.height * 0.25,
+        decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(
+                  5.0, // horizontal, move right 10
+                  5.0, // vertical, move down 10
+                ),
+              )
+            ],
+            borderRadius: BorderRadius.circular(10.0),
+            color: primary),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            //Image.asset('images/male.png',height: 100.0,width: 100.0,),
+            SizedBox(
+              height: 40.0,
+            ),
+            Text('SMS', style: headlines),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Off",
+                  style: TextStyle(
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 50.0)),
             ),
           ],
         ),
