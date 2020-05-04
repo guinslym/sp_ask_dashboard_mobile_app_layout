6accf9ed74e52d794f3944987d99dfcd24abb027
Guinsly Mondesir <guinslym@gmail.com>
2020-05-04 17:17:17 -0400

    format

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -288,46 +288,51 @@ class _MyAppState extends State<MyApp> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text('Operator currently online', style: headlineTitle),
+                Text('Currently online', style: headlineTitle),
               ],
             ),
-
-
-
-
-
-
-Expanded(
-  flex: 1,
-  child:           Container(
-            width: double.infinity,
-            //height: 100.0,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: operators.length + 1,
-              itemBuilder: (BuildContext context, int index) {
-                if (index == 0) {
-                  return SizedBox(width: 10.0);
-                }
-                
-
-
-                
-              },
+            Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    margin: EdgeInsets.all(10.0),
+                    //height: MediaQuery.of(context).size.height * 0.25,
+                    decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(
+                              5.0, // horizontal, move right 10
+                              5.0, // vertical, move down 10
+                            ),
+                          )
+                        ],
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: primary),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        //Image.asset('images/male.png',height: 100.0,width: 100.0,),
+                        SizedBox(
+                          height: 40.0,
+                        ),
+                        Text('Operators', style: headlines),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text("5",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 50.0)),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-  
-  ),
-
           ],
-
-
-
-
         ),
-
-
-
       ),
     );
   }
