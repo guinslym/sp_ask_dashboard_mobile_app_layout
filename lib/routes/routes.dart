df786c535d0055518f029a87707f8754b23faac8
Guinsly Mondesir <guinslym@gmail.com>
2020-05-13 21:44:23 -0400

    routes added

---
+++ lib/routes/routes.dart
@@ -0,0 +1,7 @@
+String operators = "/operators";
+String operatorsOnline = "/operatorsOnline";
+String queues = "/queues";
+String queuesDetails = "/queuesDetails";
+String operatorDetails = "/operatorDetails";
+String homepage = "/";
+String errorPage = "/errorPage";
